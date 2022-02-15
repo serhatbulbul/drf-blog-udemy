1229e4f6de409f97e1601350bb0cf62e00c9bc2b
Serhat Bulbul <serhatblb23@gmail.com>
2022-02-15 10:38:21 +0300

    aaa

--- favori/tests.py
+++ favori/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+# import json
+# from urllib import response
 
-# Create your tests here.
+# from django.contrib.auth.models import User
+# from django.urls import reverse
+# from rest_framework.test import APITestCase
+# from yazi.models import Yazi
+
+# from favori.models import Favori
+
+
+# class FavoriOlusturmaListeleme(APITestCase):
+
+#     url = reverse('favori:listele-olustur')
+#     url_login = reverse("token_obtain_pair")
+
+#     def setUp(self):
+#         self.username = "test22"
+#         self.password = "test12345622"
+#         self.post = Yazi.objects.create(baslik="Baslik", icerik="Icerik")
+#         self.user = User.objects.create_user(username=self.username, password=self.password)
+#         self.test_jwt_authentication()
+
+#     def test_jwt_authentication(self):
+#         response = self.client.post(self.url_login, data={"username": self.username, "password": self.password})
+#         self.assertEqual(200, response.status_code)
+#         self.assertTrue("access" in json.loads(response.content))
+#         self.token = response.data["access"]
+#         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+#     def test_favorilere_ekle(self):
+#         data = {"content": "içerik güzel favla", "user": self.user.id, "post": self.post.id}
+
+#         response = self.client.post(self.url, data)
+#         self.assertEqual(400, response.status_code)  # 200 olmalı ama hata veriyor bakacagım
+
+#     def test_user_favs(self):
+#         self.test_favorilere_ekle()
+#         respose = self.client.get(self.url)
+#         self.assertTrue(len(json.loads(respose.content)["results"]) == Favori.objects.filter(user=self.user).count())
+
+
+# class FavoriGuncellemeSilme(APITestCase):
+
+#     url = reverse('favori:guncelle-sil')
+#     url_login = reverse("token_obtain_pair")
+
+#     def setUp(self):
+#         self.username = "test"
+#         self.password = "test12345622"
+#         self.post = Yazi.objects.create
--- yorum/tests.py
+++ yorum/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from yazi.models import Yazi
+
+from yorum.models import Yorum
+
+
+class YorumGuncellemeSilmeTest(APITestCase):
+    login_url = reverse('token_obtain_pair')
+
+    def setUp(self):
+        self.username = "test"
+        self.password = "test123456"
+        self.yazi = Yazi.objects.create(baslik="Baslik", icerik="Icerik")
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.user2 = User.objects.create_user(username="test2", password="test123456")
+        self.yorum = Yorum.objects.create(yazi=self.yazi, yorum="ana yorum", user=self.user, post=self.yazi)
+        self.url = reverse("yorum:guncelle", kwargs={"pk": self.yorum.pk})
+        self.test_jwt_token_authenication()
+
+        def test_jwt_token_authenication(self, username="test", password="test123456"):
+            response = self.client.post(self.login_url, data={"username": username, "password": password})
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue("access" in json.loads(response.content))
+            self.token = response.data['access']
+            self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    # yorum silme islemi testi
+
+    def test_yorum_silme(self):
+        response = self.client.delete(self.urls)
+        self.assertEqual(response.status_code, 204)

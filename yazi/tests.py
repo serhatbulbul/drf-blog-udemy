1e7f01c2c8b145dcaef3b19c771481d1e008a440
Serhat Bulbul <computerengineerserhat@gmail.com>
2022-02-16 17:19:06 +0300

    test deneme

--- yazi/tests.py
+++ yazi/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+import json
+from urllib import response
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from yazi.models import Yazi
+
+
+class YaziListelemeOlusturmaTest(APITestCase):
+    url_list = reverse("yazi:listele")
+    url_create = reverse("yazi:olustur")
+    url_login = reverse("token_obtain_pair")
+
+    def setUp(self):
+        self.username = "test"
+        self.password = "test123456"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self):
+        response = self.client.post(self.url_login, data={"username": self.username, "password": self.password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    def test_yeni_yazi_ekleme(self):
+        data = {"baslik": "Test Yazi", "icerik": "Test Icerik"}
+        response = self.client.post(self.url_create, data)
+        self.assertEqual(201, response.status_code)
+
+    def test_giris_yapmamis_kullanici(self):
+        self.client.credentials()
+        data = {"baslik": "Test Yazi", "icerik": "Test Icerik"}
+        response = self.client.post(self.url_create, data)
+        self.assertEqual(401, response.status_code)
+
+    def test_yazi_listeleme(self):
+        response = self.client.get(self.url_list)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(len(json.loads(response.content)) > 0)
+
+
+class YaziGuncellemeSilme(APITestCase):
+    login_url = reverse("token_obtain_pair")
+
+    def setUp(self):
+        self.username = "test"
+        self.password = "test123456"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.user2 = User.objects.create_user(username="test2", password="test123456")
+        self.yazi = Yazi.objects.create(baslik="Baslik", icerik="Icerik")
+        self.url = reverse("yazi:guncelle", kwargs={"slug": self.yazi.slug})
+        self.test_jwt_token_authentication()
+
+    def test_jwt_token_authentication(self):
+        response = self.client.post(self.login_url, data={"username": self.username, "password": self.password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    # silme islemi
+    def test_yazi_silme(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(204, response.status_code)
+
+    def test_farkli_kullanici_yazi_silme(self):
+        self.test_jwt_token_authentication(self.user2)
+        response = self.client.delete(self.url)
+        self.assertEqual(403, response.status_code)
--- yorum/tests.py
+++ yorum/tests.py
@@ -1,35 +1,43 @@
-import json
+# import json
 
-from django.contrib.auth.models import User
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from yazi.models import Yazi
+# from django.contrib.auth.models import User
+# from django.urls import reverse
+# from rest_framework.test import APITestCase
+# from yazi.models import Yazi
 
-from yorum.models import Yorum
+# from yorum.models import Yorum
 
 
-class YorumGuncellemeSilmeTest(APITestCase):
-    login_url = reverse('token_obtain_pair')
+# class YorumGuncellemeSilmeTest(APITestCase):
+#     login_url = reverse('token_obtain_pair')
 
-    def setUp(self):
-        self.username = "test"
-        self.password = "test123456"
-        self.yazi = Yazi.objects.create(baslik="Baslik", icerik="Icerik")
-        self.user = User.objects.create_user(username=self.username, password=self.password)
-        self.user2 = User.objects.create_user(username="test2", password="test123456")
-        self.yorum = Yorum.objects.create(yazi=self.yazi, yorum="ana yorum", user=self.user, post=self.yazi)
-        self.url = reverse("yorum:guncelle", kwargs={"pk": self.yorum.pk})
-        self.test_jwt_token_authenication()
+#     def setUp(self):
+#         self.username = "test"
+#         self.password = "test123456"
+#         self.yazi = Yazi.objects.create(baslik="Baslik", icerik="Icerik")
+#         self.user = User.objects.create_user(username=self.username, password=self.password)
+#         self.user2 = User.objects.create_user(username="test2", password="test123456")
+#         self.yorum = Yorum.objects.create(yazi=self.yazi, ana_yorum="ana_yorum", user=self.user, post=self.yazi)
+#         self.url = reverse("yorum:guncelle", kwargs={"pk": self.yorum.pk})
+#         self.test_jwt_token_authenication()
 
-        def test_jwt_token_authenication(self, username="test", password="test123456"):
-            response = self.client.post(self.login_url, data={"username": username, "password": password})
-            self.assertEqual(response.status_code, 200)
-            self.assertTrue("access" in json.loads(response.content))
-            self.token = response.data['access']
-            self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+#         def test_jwt_token_authenication(self, username="test", password="test123456"):
+#             response = self.client.post(self.login_url, data={"username": username, "password": password})
+#             self.assertEqual(response.status_code, 200)
+#             self.assertTrue("access" in json.loads(response.content))
+#             self.token = response.data['access']
+#             self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
 
-    # yorum silme islemi testi
+#     # # # # yorum silme islemi testi
 
-    def test_yorum_silme(self):
-        response = self.client.delete(self.urls)
-        self.assertEqual(response.status_code, 204)
+#     # # # def test_yorum_silme(self):
+#     # # #     response = self.client.delete(self.urls)
+#     # # #     self.assertEqual(response.status_code, 204)
+#     # # #     self.assertFalse(Yorum.objects.filter(pk=self.ana_yorum.pk).exists())
+
+#     # # # # kendimizin olmayan bir degeri silmek
+#     # # # def test_baskasinin_yorumu_silme(self):
+#     # # #     self.test_jwt_token_authenication("test2")
+#     # # #     response = self.client.delete(self.url)
+#     # # #     self.assertEqual(response.status_code, 403)
+#     # # #     self.assertTrue(Yorum.objects.get(pk=self.ana_yorum.pk))
